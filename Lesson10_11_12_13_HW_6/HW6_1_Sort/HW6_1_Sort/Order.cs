acb9995c992aeab372e3c4d6bafbbf27885c3492
Ilia <ilyapanichenko@gmail.com>
2026-07-27 12:01:42 +0300

    HW6_Lesson10-13

---
+++ Lesson10_11_12_13_HW_6/HW6_1_Sort/HW6_1_Sort/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace HW6_1_Sort;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalAmount { get; set; }
+    public string Status { get; set; } // "Completed", "Cancelled", "Pending"
+
+    public Order(int id, int customerId, DateTime orderDate, decimal totalAmount, string status)
+    {
+        Id = id;
+        CustomerId = customerId;
+        OrderDate = orderDate;
+        TotalAmount = totalAmount;
+        Status = status;
+    }
+}
